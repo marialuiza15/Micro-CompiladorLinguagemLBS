9117d69ff3164ee8b065c8ba5dbc258a4f723c87
Maria (Malu) Luiza <148253498+marialuiza15@users.noreply.github.com>
2025-11-16 05:18:40 +0000

    up

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Micro-CompiladorLinguagemLBS
 Trabalho prático apresentado à disciplina de INF1018 - Software Básico (Turma 3WB), como parte dos requisitos necessários para avaliação parcial no período 2025.2, sob orientação do Prof. Alexandre Malheiros Meslin
+mov 
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "gera_codigo.h"
+#include <sys/mman.h>  // Para mmap
 
 int main(int argc, char *argv[]) {
     (void)argc; (void)argv;
@@ -12,12 +13,18 @@ int main(int argc, char *argv[]) {
         if (!fp) { perror("tmpfile"); return 1; }
     }
 
-    unsigned char code[300];  
+    // Aloca 1MB de memória para armazenar as funções
+    unsigned char *code = (unsigned char*)mmap(NULL, 1024 * 1024, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (code == MAP_FAILED) {
+        perror("Falha na alocação de memória");
+        exit(1);
+    }
+
     funcp funcLBS = NULL;
 
     gera_codigo(fp, code, &funcLBS);
 
-    int res = (*funcLBS)(12345);
+    int res = (*funcLBS)(10);
     printf("Resultado = %d\n", res);
 
     fclose(fp);

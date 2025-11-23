eb779fcdbbe731ce7106a3801086c89e3cc0fe9d
Maria (Malu) Luiza <148253498+marialuiza15@users.noreply.github.com>
2025-11-23 00:17:48 +0000

    upd: add prologo e epilogo

--- gera_codigo.c
+++ gera_codigo.c
@@ -8,6 +8,18 @@ void add_prologo(unsigned char code[]) {
     memcpy(code, prologo, sizeof(prologo)); 
 }
 
+void add_epilogo(unsigned char code[]) {
+    unsigned char epilogo[2] = {0xC9, 0xC3};
+    memcpy(code, epilogo, sizeof(epilogo)); 
+}
+
+void retorna_const(unsigned char code[],int i, int valor) {
+    code[i++] = 0xB8;
+    memcpy(&code[i], &valor, 4); 
+    i += 4;
+    code[i++] = 0xC3;
+}
+
 void gera_codigo (FILE *f, unsigned char code[], funcp *entry) {
     char linha[100];
     int valor = 0;
@@ -16,16 +28,19 @@ void gera_codigo (FILE *f, unsigned char code[], funcp *entry) {
 
     while (fgets(linha, sizeof(linha), f)) {
         linha[strcspn(linha, "\n")] = 0;
-        if (sscanf(linha, "ret $%d", &valor) == 1) {
+        if (strstr(linha, "function\n") != NULL) {
             add_prologo(code);
             encontrou_ret = 1;
         }
+        if (sscanf(linha, "ret $%d", &valor) == 1) {
+            retorna_const(code,i, valor);
+        }
+        if (strstr(linha, "end\n") != NULL) {
+            add_epilogo(code);
+        }
     }
 
-    code[i++] = 0xB8;
-    memcpy(&code[i], &valor, 4); 
-    i += 4;
-    code[i++] = 0xC3;
+
 
     *entry = (funcp)code;
 }
